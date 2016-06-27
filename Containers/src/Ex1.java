267a7d4c42fcdded3321adf676afc95f4e328b57
chmelvv <viktor.chmel@gmail.com>
2016-06-27 17:51:04 +0300

    init

---
+++ Containers/src/Ex1.java
@@ -0,0 +1,11 @@
+// containers/Ex1.java
+// TIJ4 Chapter Containers, Exercise 1, page 809
+/* Create a List (try both ArrayList and LinkedList) and fill it using
+* Countries. Sort the list and print it, then apply Collections.shuffle()
+* to the list repeatedly, printing it each time so that you can see how
+* the shuffle() method randomizes the list differently each time.
+*/
+
+public class Ex1 {
+
+}
