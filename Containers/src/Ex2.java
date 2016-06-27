eb2a1f62462796cd0448b4de5909af9fa17f9f9a
chmelvv <viktor.chmel@gmail.com>
2016-06-27 18:27:44 +0300

    init

--- Containers/src/Ex2.java
+++ Containers/src/Ex2.java
@@ -2,6 +2,7 @@
 // TIJ4 Chapter Containers, Exercise 2, page 809
 // Produce a Map and a Set containing all the countries that begin
 // with 'A'.
+// Answer - http://greggordon.org/java/tij4/containers/Ex2.java
 
 public class Ex2 {
 }
