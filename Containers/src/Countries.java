f16dd700a717bf9867d66544793a09569d177513
chmelvv <viktor.chmel@gmail.com>
2016-06-27 18:22:33 +0300

    init

---
+++ Containers/src/Countries.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Viktor_Chmel on 6/27/2016.
+ */
+public enum Countries {
+	Canada,
+	France,
+	Poland,
+	USA,
+	Ukraine
+}
--- Containers/src/Ex1.java
+++ Containers/src/Ex1.java
@@ -6,8 +6,28 @@
 * the shuffle() method randomizes the list differently each time.
 */
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Ex1 {
 	public static void main(String[] args) {
+
+		List<Countries> countriesList = new ArrayList<>();
+		for (Countries c: Countries.values()) {
+			countriesList.add(c);
+		}
+
+		System.out.println(countriesList);
+
+		Collections.shuffle(countriesList);
+		System.out.println(countriesList);
+
+		Collections.sort(countriesList); //sorted list as defined in enum Countries
+		System.out.println(countriesList);
+
+		Collections.shuffle(countriesList);
+		System.out.println(countriesList);
 
 	}
 
